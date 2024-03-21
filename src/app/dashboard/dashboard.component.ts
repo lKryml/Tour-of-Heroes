be67ed9982f423efac5c45673873632246fbb5a3
Krym <gumimash@gmail.com>
2024-03-21 18:24:45 +0200

    dashboard comp -- getHero() -- routing

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../_services/hero.service';
+import { take } from 'rxjs';
+import { Hero } from '../_entities/hero';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) {}
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService
+      .getHeroes()
+      .pipe(take(1))
+      .subscribe((heroes) => (this.heroes = heroes.slice(1, 5)));
+  }
 }
